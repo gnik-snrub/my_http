ae53e3d8bef4d471e5e128552b0a304bd103b5e8
Josiah Morris <josiahbmorris@hotmail.com>
2025-05-06 05:07:10 +0900

    Added core listen loop to main()

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,29 @@
+use std::net::TcpListener;
+mod connection;
+use connection::handle_client;
 
+fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("127.0.0.1:7878");
+
+    match listener {
+        Ok(l) => {
+            println!("Listening on 127.0.0.1:7878");
+            for stream in l.incoming() {
+                match stream {
+                    Ok(mut s) => {
+                        handle_client(&mut s);
+                    },
+                    Err(e) => {
+                        println!("Error in stream: {:?}", e);
+                    }
+                }
+            }
+        },
+        Err(e) => {
+            println!("Error binding listener: {:?}", e);
+            std::process::exit(1);
+        }
+    }
+
+    Ok(())
+}
