d8337c7189f9a1d5ea79bc76a4269e71ba087aa0
Josiah Morris <josiahbmorris@hotmail.com>
2025-05-23 00:00:18 +0900

    Updated main to use tokio TcpListener, and spawn threads around client handling

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,16 @@
-use std::net::TcpListener;
+use tokio::net::TcpListener;
 mod connection;
 use connection::handle_client;
 mod parser;
 mod router;
 mod response;
 
-fn main() -> std::io::Result<()>{
-    let listener = TcpListener::bind("127.0.0.1:7878");
+#[tokio::main] async fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("127.0.0.1:7878").await?;
+    println!("Listening on 127.0.0.1:7878");
 
-    match listener {
-        Ok(l) => {
-            println!("Listening on 127.0.0.1:7878");
-            for stream in l.incoming() {
-                match stream {
-                    Ok(mut s) => {
-                        handle_client(&mut s);
-                    },
-                    Err(e) => {
-                        println!("Error in stream: {:?}", e);
-                    }
-                }
-            }
-        },
-        Err(e) => {
-            println!("Error binding listener: {:?}", e);
-            std::process::exit(1);
-        }
+    while let Ok((socket, addr)) = listener.accept().await {
+        tokio::spawn(handle_client(socket));
     }
 
     Ok(())
