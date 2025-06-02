e4d1f5391bc42d31e0640807b8250fa95d9e89dd
Josiah Morris <josiahbmorris@hotmail.com>
2025-06-03 03:17:05 +0900

    Setup rolling logging subscriber to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use tokio::net::TcpListener;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Layer };
+use tracing_appender::rolling;
+
 mod connection;
 use connection::handle_client;
 mod parser;
@@ -6,6 +10,19 @@ mod router;
 mod response;
 
 #[tokio::main] async fn main() -> std::io::Result<()>{
+    let file_appender = rolling::daily("logs", "server.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let file_layer = fmt::Layer::default()
+        .json()
+        .with_writer(non_blocking)
+        .with_timer(fmt::time::UtcTime::rfc_3339())
+        .with_filter(LevelFilter::INFO);
+
+    tracing_subscriber::registry()
+        .with(file_layer)
+        .init();
+
     let listener = TcpListener::bind("127.0.0.1:7878").await?;
     println!("Listening on 127.0.0.1:7878");
 
