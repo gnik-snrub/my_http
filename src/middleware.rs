8bf385ef1bcbef81c7d6603947a88769aea4b325
Josiah Morris <josiahbmorris@hotmail.com>
2025-06-05 03:10:57 +0900

    Added Middleware trait

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,1 +1,15 @@
+use std::{future::Future, pin::Pin};
 
+use async_trait::async_trait;
+
+use crate::{parser::Request, response::Response};
+#[async_trait]
+trait Middleware: Send + Sync {
+    async fn handle(
+        &self,
+        req: Request,
+        next: Box<dyn FnOnce(Request) -> ResponseFuture + Send>,
+    ) -> Response;
+}
+
+type ResponseFuture = Pin<Box<dyn Future<Output = Response> + Send>>;
