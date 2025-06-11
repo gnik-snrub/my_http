c689ca68abf476aa08cbbc6b60f9501c9bac30f6
Josiah Morris <josiahbmorris@hotmail.com>
2025-06-12 04:16:44 +0900

    Added directory handling to file_serving module

--- src/file_serving.rs
+++ src/file_serving.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{collections::HashMap, fs::{self}, path::PathBuf};
+use serde_json::json;
 
 use crate::{parser::Request, response::{Response, StatusCode}};
 
@@ -7,23 +8,46 @@ pub async fn serve_file(req: &Request, res: Response) -> Response {
     let file_name = path_split[path_split.len() - 1];
     let rel_path = PathBuf::from("public").join(file_name);
 
-    return match tokio::fs::read(&rel_path).await {
-        Ok(bytes) => {
-            let mime_type = get_mime_type(file_name);
-            let mut file_res = res.status(StatusCode::Ok).text(&"")
-                .header("Content-Type", mime_type)
-                .header("Content-Length", bytes.len().to_string().as_str());
-            file_res.body = bytes;
-            file_res
-        }
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
-            println!("{:?}", e);
-            res.status(StatusCode::NotFound).text(&"404 Not Found")
-        }
-        Err(_) => {
-            res.status(StatusCode::InternalError).text(&"500 Internal Error")
+    let metadata = match tokio::fs::metadata(&rel_path).await {
+        Ok(m) => m,
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => return res.status(StatusCode::NotFound).text(&"404 Not Found"),
+        Err(_) => return res.status(StatusCode::InternalError).text(&"500 Internal Error")
+    };
+
+    if metadata.is_file() {
+        return match tokio::fs::read(&rel_path).await {
+            Ok(bytes) => {
+                let mime_type = get_mime_type(file_name);
+                let mut file_res = res.status(StatusCode::Ok).text(&"")
+                    .header("Content-Type", mime_type)
+                    .header("Content-Length", bytes.len().to_string().as_str());
+                file_res.body = bytes;
+                file_res
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+                println!("{:?}", e);
+                res.status(StatusCode::NotFound).text(&"404 Not Found")
+            }
+            Err(_) => {
+                res.status(StatusCode::InternalError).text(&"500 Internal Error")
+            }
         }
     }
+
+    if metadata.is_dir() {
+        let mut vec = vec![];
+        if let Ok(entry) = fs::read_dir(&rel_path) {
+            entry.for_each(|x| 
+                if let Ok(item) = x {
+                    let name = item.file_name().to_string_lossy().into_owned();
+                    vec.push(name);
+                }
+            );
+        }
+        return res.status(StatusCode::Ok).json(&json!(&vec));
+    }
+    res.status(StatusCode::InternalError).text(&"500 Internal Error")
+
 }
 
 fn get_mime_type(file_name: &str) -> &str {
