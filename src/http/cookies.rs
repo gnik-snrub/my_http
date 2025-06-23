8f58b767dfde6fdfc0a7c6bc1bcc50a35c18ace5
Josiah Morris <josiahbmorris@hotmail.com>
2025-06-24 04:59:56 +0900

    Added Cookie struct, as well as new and serialize implementations

---
+++ src/http/cookies.rs
@@ -0,0 +1,34 @@
+use chrono::{DateTime, Duration, Utc};
+
+pub struct Cookie {
+    name: String,
+    value: String,
+    path: String,
+    expires: DateTime<Utc>,
+    http_only: bool,
+    secure: bool,
+}
+
+impl Cookie {
+    pub fn new(
+        name: String,
+        value: String,
+        ) -> Cookie {
+        Cookie {
+            name,
+            value,
+            path: String::from("/"),
+            expires: Utc::now() + Duration::days(1),
+            http_only: true,
+            secure: true,
+        }
+    }
+
+    pub fn serialize(&self) -> String {
+        let mut result = String::from(format!("{}={}; Path={}; Expires={};", self.name, self.value, self.path, self.expires));
+        if self.http_only { result.push_str(" HttpOnly;"); }
+        if self.secure { result.push_str(" Secure;"); }
+
+        result
+    }
+}
