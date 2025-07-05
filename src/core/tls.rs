7f960d99d9f5ae4ca214dac3d7c7a3594c5d1a09
Josiah Morris <josiahbmorris@hotmail.com>
2025-07-06 03:10:42 +0900

    Refactored load_certs_and_keys function in tls.rs to use environment variables

--- src/core/tls.rs
+++ src/core/tls.rs
@@ -4,8 +4,12 @@ use rustls::{pki_types::{CertificateDer, PrivateKeyDer}};
 use rustls_pemfile::{certs, private_key};
 
 pub fn load_certs_and_key() -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>), std::io::Error>{
-    let mut cert_file = &mut BufReader::new(File::open("tls/cert.pem").unwrap());
-    let mut key_file = &mut BufReader::new(File::open("tls/key.pem").unwrap());
+    dotenvy::dotenv().ok();
+    let cert_path = std::env::var("TLS_CERT_PATH").expect("TLS_CERT_PATH not set");
+    let key_path = std::env::var("TLS_KEY_PATH").expect("TLS_KEY_PATH not set");
+
+    let mut cert_file = &mut BufReader::new(File::open(cert_path).unwrap());
+    let mut key_file = &mut BufReader::new(File::open(key_path).unwrap());
 
     let cert: Vec<CertificateDer<'static>> = certs(&mut cert_file).collect::<Result<_,_>>()?;
     let key: PrivateKeyDer<'static> = private_key(&mut key_file)?
